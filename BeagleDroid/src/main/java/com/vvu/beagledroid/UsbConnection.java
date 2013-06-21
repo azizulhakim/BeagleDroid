6f6d60378815386a3d2af56bb01d78857206efe3
Vlad Victor Ungureanu <ungureanuvladvictor@gmail.com>
2013-06-21 19:02:53 +0300

    RNDIS + Ethernet2 support added

--- BeagleDroid/src/main/java/com/vvu/beagledroid/UsbConnection.java
+++ BeagleDroid/src/main/java/com/vvu/beagledroid/UsbConnection.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Vlad V. Ungureanu <ungureanuvladvictor@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this Github repository and wiki except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.vvu.beagledroid;
 
 import android.app.Activity;
@@ -136,13 +152,12 @@ public class UsbConnection extends MainActivity {
 		boolean me = true;
 		int tmp = -1;
 		while (me) {
-			tmp = myConnection.bulkTransfer(endIN, buffer, 500, 70);
+			tmp = myConnection.bulkTransfer(endIN, buffer, 500, 0);
+			Log.d(TAG, "Received " + tmp + ".");
+			HexDump myDumper = new HexDump();
+			Log.d(TAG, myDumper.dumpHexString(buffer));
 			if (tmp >= 0) me = false;
 		}
-
-		Log.d(TAG, "Received " + tmp + ".");
-		HexDump myDumper = new HexDump();
-		Log.d(TAG, myDumper.dumpHexString(buffer));
 	}
 }
 
