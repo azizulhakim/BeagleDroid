ff8db402c6237223c2a59ed52e24ef820b915a8d
Vlad Victor Ungureanu <ungureanuvladvictor@gmail.com>
2013-09-08 15:06:43 +0200

    Added license information

--- src/com/vvu/beagledroid/IPv4.java
+++ src/com/vvu/beagledroid/IPv4.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Vlad V. Ungureanu <ungureanuvladvictor@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this Github repository and wiki except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.vvu.beagledroid;
 
 import java.nio.ByteBuffer;
@@ -138,12 +154,9 @@ public class IPv4 {
 		    long sum = 0;
 		    long data;
 
-		    // Handle all pairs
 		    while (length > 1) {
-		      // Corrected to include @Andy's edits and various comments on Stack Overflow
 		      data = (((buf[i] << 8) & 0xFF00) | ((buf[i + 1]) & 0xFF));
 		      sum += data;
-		      // 1's complement carry bit correction in 16-bits (detecting sign extension)
 		      if ((sum & 0xFFFF0000) > 0) {
 		        sum = sum & 0xFFFF;
 		        sum += 1;
@@ -153,18 +166,13 @@ public class IPv4 {
 		      length -= 2;
 		    }
 
-		    // Handle remaining byte in odd length buffers
 		    if (length > 0) {
-		      // Corrected to include @Andy's edits and various comments on Stack Overflow
 		      sum += (buf[i] << 8 & 0xFF00);
-		      // 1's complement carry bit correction in 16-bits (detecting sign extension)
 		      if ((sum & 0xFFFF0000) > 0) {
 		        sum = sum & 0xFFFF;
 		        sum += 1;
 		      }
 		    }
-
-		    // Final 1's complement value correction to 16-bits
 		    sum = ~sum;
 		    sum = sum & 0xFFFF;
 		    return (short)sum;
